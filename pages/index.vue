84c2faa22890e665ff95ad9b47a3b56b640102be
Houcine <swidyhoussine@gmail.com>
2024-07-22 12:47:37 +0100

    initial commit🚀

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,6 @@
     <div class="flex justify-center items-center h-32">
       <input
         class="px-2 py-1 border border-gray-800 rounded-md min-w-64"
-        v-model="searchTerm"
         type="text"
         placeholder="Search..."
       />
@@ -17,20 +16,23 @@
   <div
     class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 self-center gap-x-10 mb-10"
   >
-    <div v-for="movie in data?.results">
+    <!-- <div v-for="movie in data?.results">
       {{ movie.title }}
-    </div>
+    </div> -->
+    {{ product }}
   </div>
 </template>
 
 <script setup lang="ts">
-import type { APIResponse } from '@/types/APIResponse';
+// import type { APIResponse } from '@/types/APIResponse';
 
-const searchTerm = ref("");
+// const searchTerm = ref("");
 
-const url = computed(() => {
-  return `api/movies/search?query=${searchTerm.value}`;
-});
+// const url = computed(() => {
+//   return `api/movies/search?query=${searchTerm.value}`;
+// });
 
-const { data } = await useFetch<APIResponse>(url);
+// const { data } = await useFetch<APIResponse>(url);
+
+const { data: product } = await useFetch("https://dummyjson.com/products/1");
 </script>
